9fb63e59c9865da14543b1bbfbc96511750ab0f2
Monesh-P <166040672+Monesh-P@users.noreply.github.com>
2026-02-03 12:29:52 +0530

    Update pe_mac_tb.v

--- weekly_reports/Week_1/code/pe_mac_tb.v
+++ weekly_reports/Week_1/code/pe_mac_tb.v
@@ -1,20 +1,36 @@
 module pe_mac_tb;
+
     logic clk, rst, valid;
     logic [7:0] a, b;
     logic [15:0] acc;
 
-    pe_mac dut (.clk(clk), .rst(rst), .valid(valid), .a(a), .b(b), .acc(acc));
+    pe_mac dut (
+        .clk(clk),
+        .rst(rst),
+        .valid(valid),
+        .a(a),
+        .b(b),
+        .acc(acc)
+    );
 
     always #5 clk = ~clk;
 
     initial begin
-        clk = 0; rst = 1; valid = 0;
-        a = 0; b = 0;
+        clk = 0;
+        rst = 1;
+        valid = 0;
+        a = 0;
+        b = 0;
+
         #10 rst = 0;
 
+        // MAC operation
         a = 8'd2; b = 8'd3; valid = 1; // acc = 6
-        #10 valid = 0;                // hold
-        #10 a = 8'd4; b = 8'd5; valid = 1; // acc = 26
+        #10 valid = 0;                 // hold
+
+        // Another MAC
+        a = 8'd4; b = 8'd5; valid = 1; // acc = 26
         #20 $finish;
     end
+
 endmodule
